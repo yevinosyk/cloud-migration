5bc1e3113e36102498ef260edab849a50f52448e
Yevheniya <yevheniya.nosyk@gmail.com>
2018-01-14 18:21:08 +0200

    Added getNewForm method

--- APP_ROOT/silex_web/src/Controllers/NodesController.php
+++ APP_ROOT/silex_web/src/Controllers/NodesController.php
@@ -3,7 +3,9 @@
     namespace Keywords\Controllers;
 
     class Nodes {
-        
+        public function getNewForm(Application $app): string {
+            return $app['twig']->render('new_keyword.twig');
+        }
     }
 
 ?>
